0d457b8fcabcb09116b25445e08f931da130b75c
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2023-08-21 10:01:41 +0200

    Update clocks.js

--- js/clocks.js
+++ js/clocks.js
@@ -1,26 +1,26 @@
-// Определяем функцию отображения текущего времени
+// Defining the current time display function
 function clockTimer2 () { 
-    // Получаем текущее время
+    // Getting the current time
     let date = new Date(); 
-    // Определяем стиль форматирования информации о времения для вывода
+    // // Defining the formatting style of the time information for output
     const formatter = new Intl.DateTimeFormat(
-        "en-GB",  // Стиль вывода для конкретного (английского) языка
+        "en-GB",  // Output style for a specific (English) language
     {   
-        hour: "2-digit", // вывод часов только с двумя знаками
-        minute: "2-digit", // вывод минут только с двумя знаками
-        second: "2-digit" // вывод секунд только с двумя знаками
+        hour: "2-digit", // output of the clock with only two signs
+        minute: "2-digit", // output minutes with only two characters
+        second: "2-digit" // output seconds with only two characters
     }); 
-    // Ищем елемент с id = clock
+    // Looking for an element with id = clock
     let clock = document.getElementById("clock"); 
-    // Тень для текста времени
+    // Shadow for time text
     clock.style.textShadow = "3px 5px 3px #c8daf5";
-    // Устанавливаем в элемент clock форматированное значение времени
+    // Setting the formatted time value to the clock element
     clock.innerHTML = formatter.format(date); 
-    // Определяем массив названий дней недели для английского языка
+    // Defining an array of names of days of the week for the English language
     let dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
-    // Ссылка на элемент DOM с id = dayOfWeekWrapper
+    // Link to the DOM element with id = dayOfWeekWrapper
     let dayOfWeekDomElement = document.getElementById("dayOfWeekWrapper"); 
-    // Устанавливаем значение с названием дня недели
+    // Setting the value with the name of the day of the week
     dayOfWeekDomElement.innerHTML = dayOfWeek[date.getDay()]; 
 }
 
